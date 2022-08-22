5f3791160ed41558e3f9652b7fd34696aab8906a
{css3studio} <css3studio@gmail.com>
2022-08-22 17:44:39 +0800

    tailwind 적용

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/* Tailwind CSS IntelliSense를 위한 설정파일 */
+
+module.exports = {
+  content: ["./html/*.html"],
+  theme: {
+    container: {
+      center: true,
+      screens: {
+        sm: "100%",
+        md: "100%",
+        lg: "1168px",
+        xl: "1168px"
+      },
+      padding: {
+        DEFAULT: "16px",
+        sm: "16px",
+        md: "16px",
+        lg: "0px",
+        xl: "0px"
+      }
+    },
+    extend: {
+      colors: {
+        'primary': '#D03B83',
+        'black111': '#111',
+        'gray600': '#666',
+        'pink300': '#E7A3C4',
+      },
+    }
+  },
+}
